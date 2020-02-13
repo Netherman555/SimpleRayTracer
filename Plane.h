f55b6ebc45cbf502c5ea653ae1087909769fb63a
Netherman555 <netherman555@gmail.com>
2020-02-13 13:48:16 -0500

    Create Plane.h

---
+++ Plane.h
@@ -0,0 +1,22 @@
+//Basic plane equation class
+
+#include "Vec3f.h"
+
+class Plane
+{
+  private:
+    float x, y, z, d;
+  public:
+    Plane(Vec3f a, Vec3f b, Vec3f c); //Makes an equation from three points
+    Plane(float a, float b, float c, float d); //Makes a Plane object from an equation itself.
+
+    //Get Methods
+    float getX();
+    float getY();
+    float getZ();
+    float getD();
+
+    //Collision Methods
+    
+
+};
