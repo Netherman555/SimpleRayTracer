dda2d50ae82e56bfa96c6c98d396225d1de3cdcc
Netherman555 <netherman555@gmail.com>
2020-02-13 13:47:18 -0500

    Update Vec3f.cpp

--- Vec3f.cpp
+++ Vec3f.cpp
@@ -1,4 +1,5 @@
 #include "Vec3f.h"
+#include <iostream>
 
 //Creates a vector with 3 floats
 Vec3f::Vec3f(float X, float Y, float Z)
@@ -32,4 +33,22 @@ float Vec3f::getY()
 float Vec3f::getZ()
 {
   return z;
+}
+
+float Vec3f::dot(Vec3f vector)
+{
+  float newX = x * vector.getX();
+  float newY = y * vector.getY();
+  float newZ = z * vector.getZ();
+
+  return newX + newY + newZ; 
+}
+
+Vec3f Vec3f::cross(Vec3f vector)
+{
+  float newX = (y * vector.getZ()) - (z * vector.getY());
+  float newY = ((x * vector.getZ()) - (z * vector.getX())) * -1;
+  float newZ = (x * vector.getY()) - (y * vector.getX());
+
+  return Vec3f(newX, newY, newZ);
 }
