bc1a619cfd4bacbffe147da436c6741fd1ad5453
Netherman555 <netherman555@gmail.com>
2020-02-14 08:33:48 -0500

    Update Camera.h

--- Camera.h
+++ Camera.h
@@ -1,20 +1,24 @@
-#pragma once
 #include "Ray.h"
 #include "Vec3f.h"
+#include <math.h>
+#include <vector>
+#include <iostream>
 
 class Camera
 {
-private:
-	Vec3f cameraPoint;
-	Vec3f direction;
-	int fov;
-	int clipDistance;
+  private:
+    Vec3f cameraPoint; //Camera origin point
+    Vec3f cameraDirection; //Direction vector of camera
+    int xFov, yFov; //FOV, in degrees, in x and y axis
+    int clippingDistance; //Distance from camera point to camera plane
+    int resolutionX, resolutionY; //Resolution in x and y axis
 
-	int cameraPlaneWidth;
-	int cameraPlaneHeight;
+    int planeWidth, planeHeight; //Width and height of intersect plane
 
-	double 
-public:
-	Camera(Ray cameraRay, int fov, int clippingDistance); //Defines a camera using ray as the starting position and direction.
-	Camera(); //Defines a camera with a starting position of (0, 0, 0) and a direction of (1, 0, 0)
+    std::vector<Vec3f> pixelLocations;
+  public:
+    Camera(Ray cameraRay, int xFOV, int yFOV, double ClippingDistance, int ResolutionX, int ResolutionY); //Creates a camera and arranges all pixel locations.
+
+    //Rendering functions
+    
 };
