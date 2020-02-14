486d7506f93c0d57f0353c9e7f2b799899baf81c
Netherman555 <netherman555@gmail.com>
2020-02-13 19:17:48 -0500

    Update Ray.h

--- Ray.h
+++ Ray.h
@@ -1,10 +1,19 @@
 #include "Vec3f.h"
 
+#ifndef __RAY_INCLUDE__
+#define __RAY_INCLUDE__
+
 class Ray
 {
   private:
-    Vec3f origin;
-    Vec3f direction;
+    Vec3f pOrigin;
+    Vec3f pDirection;
   public:
     Ray(Vec3f Origin, Vec3f Direction);
-}
+
+    //Get Methods
+    Vec3f getOrigin();
+    Vec3f getDirection();
+};
+
+#endif
