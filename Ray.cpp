8978bc99fe777ac38f47fb639c8349b03439e149
Netherman555 <netherman555@gmail.com>
2020-02-13 19:17:30 -0500

    Update Ray.cpp

--- Ray.cpp
+++ Ray.cpp
@@ -1,7 +1,19 @@
 #include "Ray.h"
 
-Ray::Ray(Vec3f Origin, Vec3f Direction)
+Ray::Ray(Vec3f Origin, Vec3f PointOne)
 {
-  origin = Origin;
-  direction = Direction;
+  //Uses parametric equation x = (1-t*)Origin + t*Direction
+
+  pOrigin = Origin;
+  pDirection = Vec3f(PointOne.getX() - pOrigin.getX(), PointOne.getY() - pOrigin.getY(), PointOne.getZ() - pOrigin.getZ());
+}
+
+Vec3f Ray::getOrigin()
+{
+  return pOrigin;
+}
+
+Vec3f Ray::getDirection()
+{
+  return pDirection;
 }
