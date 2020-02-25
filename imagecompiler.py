ecf8d59ad257a9470c45863d5abf626443668edd
Netherman555 <netherman555@gmail.com>
2020-02-25 08:30:34 -0500

    Add files via upload

--- imagecompiler.py
+++ imagecompiler.py
@@ -17,18 +17,7 @@ for y in range(100):
         g = tuples[overallCount][1]
         b = tuples[overallCount][2]
 
-        if(b == -1000000000.0):
-            image.set_at((x, y), (0, 0, 0))
-
-        else:
-            if(b * 8 <= -255):
-                image.set_at((x, y), (0, 0, 255))
-
-            elif(b * 8 >= 0):
-                image.set_at((x, y), (0, 0, 0))
-
-            else:
-                image.set_at((x, y), (0, 0, abs(int(b * 8))))
+        image.set_at((x, y), (r, g, b))
 
         overallCount += 1
 
